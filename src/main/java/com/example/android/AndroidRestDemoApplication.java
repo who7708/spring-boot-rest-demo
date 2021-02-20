7fb1a11e356a2da1e6ef38af776f5c9079f7ef4c
Chris <who7708@163.com>
2021-02-20 23:47:34 +0800

    init

---
+++ src/main/java/com/example/android/AndroidRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidRestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidRestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/android/AndroidRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidRestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
